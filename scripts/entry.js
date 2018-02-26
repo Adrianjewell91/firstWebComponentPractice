3a99cbeb30654f08f2d23a9db171c9b32f26b6e6
Adrian <adrianjewell91@gmail.com>
2018-02-26 13:10:31 -0800

    used async await with shadow dom

--- scripts/entry.js
+++ scripts/entry.js
@@ -4,15 +4,28 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
 });
 
-function buildElement() {
+
+function getData() {
+  let data = fetch('https://randomuser.me/api/').then((r) => r.json())
+                                                      .then((r) => r);
+  // debugger;
+  return data;
+}
+
+async function buildElement() {
 
   const HelloWorld = document.registerElement('hello-world');
 
   var hello = new HelloWorld();
-
+  var data = await getData();
+  var results = data.results[0].login.username;
+  // debugger;
   const helloShadow = hello.attachShadow({mode: "open"});
-  helloShadow.innerHTML = "<h1>This is information in my custom element</h1>";
+  helloShadow.innerHTML = "<h1>This is a random user that from random users API:</h1>";
+  helloShadow.innerHTML += `<h1>${results}</h1>`;
 
   document.body.appendChild(hello);
-
 }
+
+
+//Make a XhR request with async await
