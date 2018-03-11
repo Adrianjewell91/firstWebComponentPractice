209d18c5eab64a7664f63808be6a2a380c3ee0de
Adrian <adrianjewell91@gmail.com>
2018-03-10 17:42:56 -0800

    made a menu starting point, time to write the panel selection functions

--- scripts/bundle/bundle.js
+++ scripts/bundle/bundle.js
@@ -83,15 +83,74 @@ document.addEventListener("DOMContentLoaded", async function(e) {
   customElements.define('airplane-seat', __WEBPACK_IMPORTED_MODULE_1__airplaneGrid_index__["b" /* AirplaneSeat */]);
   customElements.define('airplane-grid', __WEBPACK_IMPORTED_MODULE_1__airplaneGrid_index__["a" /* AirplaneGrid */]);
   customElements.define('overhead-compartment', __WEBPACK_IMPORTED_MODULE_1__airplaneGrid_index__["c" /* OverHeadCompartment */]);
+  customElements.define('tabbed-menu', TabMenu);
+
+
+  // 1: Attach the menu:
+  document.body.appendChild(document.createElement("tabbed-menu"));
 
   // 2: Build the getWeather API
   let getAirportWeather = document.createElement('airport-weather');
-  document.querySelector(".menu-drop-down").appendChild(getAirportWeather);
+  document.querySelector("tabbed-menu").attachItem(getAirportWeather);
 
   // 3: Build the overhead luggage compartment.
   let airplaneGrid = document.createElement("airplane-grid");
-  document.querySelector(".menu-drop-down").appendChild(airplaneGrid);
+  document.querySelector("tabbed-menu").attachItem(airplaneGrid);
+
 });
+
+/**
+  Holds the menu, and it's extendable. Aka. I can add tabs to it using an API.
+  When I click a tab, the corresponding div becomes active, and the tab
+  becomes highlighted, while the other one goes out.
+
+*/
+class TabMenu extends HTMLElement {
+  createShadowRoot() {
+    this.attachShadow({mode: "open"});
+    return this.shadowRoot;
+  }
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this._root = this.createShadowRoot();
+    this._root.innerHTML = `
+      <style>
+        .menu * {
+          display: inline;
+          border: 1px solid black;
+          border-bottom: 0;
+        }
+      </style>
+      <div class="menu">
+      </div>
+      <div class="menu-drop-down">
+      </div>
+    `;
+
+    this._menu = this.shadowRoot.querySelector(".menu");
+    this._dropDown = this.shadowRoot.querySelector(".menu-drop-down");
+  }
+
+  attachItem(node) {
+    debugger;
+    const button = document.createElement("button");
+    button.textContent = node.localName;
+    this._menu.appendChild(button);
+    this._dropDown.appendChild(node);
+  }
+
+  _deactivateCurrent() {
+
+  }
+
+  _activateClicked() {
+
+  }
+}
 
 
 /***/ }),
